52d503b2bd3bae12050672a4d3629d9e5517fb4f
Jose Guerra <jose@gcostudios.com>
2019-11-25 21:30:47 +0000

    Added initial script

--- jsDev/app.js
+++ jsDev/app.js
@@ -1,1 +1,26 @@
-// Add custom code
+jQuery(function($) {
+
+  'use strict';
+
+  var ENHANCEMENTS;
+
+  // ================================
+  // Enhancements
+  // ================================
+
+    ENHANCEMENTS = {
+
+    init: function() {
+      this.customScript();
+    },
+
+    // Custom Script
+    // ==============================================
+    customScript() {
+      // Add your script here
+    }
+  };
+  
+  ENHANCEMENTS.init();
+
+}); //jQuery
